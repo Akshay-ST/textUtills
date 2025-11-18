1f468099a3e1a4fc0027eed2c2144e0f9158d2ca
akshay <akshayst@aol.com>
2025-11-18 11:27:35 +0530

    git init

---
+++ src/components/Navbar.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useState, useEffect } from 'react';
+
+export default function Navbar(props) {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    useEffect(() => {
+        // Apply or remove dark mode class on the document element
+        if (isDarkMode) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+        } else {
+        document.documentElement.removeAttribute('data-bs-theme');
+        }
+    }, [isDarkMode]);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">{props.title}</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">{props.aboutText}</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Options
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="/">Action 1</a></li>
+                  <li><hr className="dropdown-divider"/></li> 
+                  <li><a className="dropdown-item" href="/">Action 2</a></li>
+                  <li><hr className="dropdown-divider"/></li>
+                  <li><a className="dropdown-item" href="/">Action 3</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+              <button className="btn btn-outline-primary" type="submit">Search</button>
+            </form>
+            <button className="navbar-toggler" type="button">
+            <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                <button 
+                    className="btn btn-outline-secondary"
+                    onClick={toggleDarkMode}
+                >
+                    {isDarkMode ? '☀️ Light' : '🌙 Dark'}
+                </button>
+                </li>
+            </ul>
+            </div>
+          </div>
+        </div>
+    </nav>
+  )
+}
+
+Navbar.propTypes = {
+    title: PropTypes.string,
+    aboutText: PropTypes.string
+}
+ 
+Navbar.defaultProps = {
+    title: "Set Title Here",
+    aboutText: "About Text Here"
+}
+
---
+++ src/components/TextForm.js
@@ -0,0 +1,59 @@
+import React, {useState} from "react";
+// import PropTypes from 'prop-types'
+
+export default function TextForm(props){
+
+    const [text, setText] = useState('');
+    // text = "new text"; // wrong way to change the state
+    // setText("new text"); // correct way to change the state  
+
+    const handleUpClick = ()=>{
+        console.log("Upper case was clicked " + text)
+        let newText = text.toUpperCase();
+        setText(newText);
+    }
+    const handleLowClick = ()=>{
+        console.log("Lower case was clicked " + text)
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const handleClear = ()=>{
+        console.log("Clear was clicked " + text)
+        let newText = ""
+        setText(newText);
+    }
+    const handleRev = ()=>{
+        console.log("Reverse String was clicked " + text)
+        let newText = text.split('').reverse().join('');
+        setText(newText);
+    }
+    
+    const handleOnChange = (event)=>{
+        console.log("Handle On Change");
+        setText(event.target.value);
+    }
+
+    return(
+        <>
+        <div className="container">
+            <h1>{props.heading}</h1>
+            <div className="mb-3">
+            <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to UpperCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
+            <button className="btn btn-primary mx-1" onClick={handleRev}>Reverse String</button>
+            <button className="btn btn-primary mx-1" onClick={handleClear}>Clear</button>
+
+        </div>
+
+        <div className="container my-3">
+            <h3>Your Text Summary</h3>
+            <p>{text.split(" ").length} words and {text.length} characters</p>
+            <p>{0.008 * text.split(" ").length} Read in minutes</p>
+            <h4>Preview</h4>
+            <p>{text}</p>
+        </div>
+        </>
+    )
+}
